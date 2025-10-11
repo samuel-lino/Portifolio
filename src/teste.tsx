d350f11022fc24f965ae15548900a1c10fbf8269
samuel <samuellino2006@gmail.com>
2025-10-11 18:51:01 -0300

    Styled components adicionado

---
+++ src/teste.tsx
@@ -0,0 +1,24 @@
+import { BlobOptions } from 'buffer'
+import styled from 'styled-components'
+
+type ButtonProps = {
+  confirmar?: boolean
+  fontsize?: string
+}
+
+const Bsaudacao = styled.button<ButtonProps>`
+  background-color: ${(props) => (props.confirmar ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontsize || '16px'};
+  border-radius: 50%;
+`
+
+function Teste() {
+  return (
+    <div>
+      <h1>Olá</h1>
+      <Bsaudacao fontsize="24px">clique aqui</Bsaudacao>
+    </div>
+  )
+}
+
+export default Teste
